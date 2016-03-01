84da6932ea8a9eca56c6736d0f7854814dd9acd7
DavidsRiver <riosdj@mcmaster.ca>
2016-03-01 14:55:38 -0500

    Update scraper.py
    Created URL Generator , that creates baseball reference url

--- scraper.py
+++ scraper.py
@@ -3,68 +3,31 @@
 import urllib
 import bs4
 
-FirstName = 'Jose"
-LastName = "Bautista"
+def urlgenerator2(firstname,lastname):
+  a = 'http://www.baseball-reference.com/players/'
+  b = lastname[0]
+  c = lastname[0:5]
+  d = firstname[0:2]
+  e = '02'
+  f = '01'
+  g = '.shtml'
+  z = '/'
+  url = a+b+z+c+d+e+g
+  print (url.lower())
 
 //http://www.baseball-reference.com/players/b/bautijo02.shtml
 
-w = LastName[0]
-x = LastName[0:3]
-y = FirstName[0:1]
-z = "02"
-
-
-
-
-// trying to create a function that prints the url like it is in baseball reference. Doesn't work so far
-
->>> print
-<built-in function print>
->>> firstnam= Jose
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    firstnam= Jose
-NameError: name 'Jose' is not defined
->>> firstname= 'jose'
->>> print firstname
-SyntaxError: invalid syntax
->>> print %firstname
-Traceback (most recent call last):
-  File "<pyshell#4>", line 1, in <module>
-    print %firstname
-TypeError: unsupported operand type(s) for %: 'builtin_function_or_method' and 'str'
->>> print 'firstname
-SyntaxError: EOL while scanning string literal
->>> x = "jose"
->>> print x
-SyntaxError: invalid syntax
->>> print (x)
-jose
->>> pip install beautifulsoup4
-SyntaxError: invalid syntax
->>> firstletter = x[0]
->>> y=
-SyntaxError: invalid syntax
->>> y="bautista"
->>> print x + y
-SyntaxError: invalid syntax
->>> print (x+y)
-josebautista
->>> print (firstletter)
-j
->>> firstletter = y[0]
->>> lastname4 =y[0:3]
->>> firstname2= x[0:1]
->>> 
->>> print("http://www.baseball-reference.com/players/%/%%02.shtml" (firstletter,lastname4,firstname2))
-Traceback (most recent call last):
-  File "<pyshell#22>", line 1, in <module>
-    print("http://www.baseball-reference.com/players/%/%%02.shtml" (firstletter,lastname4,firstname2))
-TypeError: 'str' object is not callable
->>> 
-
-
-
+def urlgenerator1(firstname,lastname)
+  a = 'http://www.baseball-reference.com/players/'
+  b = lastname[0]
+  c = lastname[0:5]
+  d = firstname[0:2]
+  e = '02'
+  f = '01'
+  g = '.shtml'
+  z = '/'
+  url = a+b+z+c+d+f+g
+  print (url.lower())
 
 page = urllib.request.urlopen("http://www.baseball-reference.com/players/b/bautijo02.shtml")
 #http://www.baseball-reference.com/players/b/bautijo02.shtml
