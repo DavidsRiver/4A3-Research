99daac0532aaf0213cf771a7c41401834de1ac70
DavidsRiver <riosdj@mcmaster.ca>
2016-03-05 22:28:36 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,33 +3,41 @@
 import urllib
 import bs4
 
-urls =[]
+names = [['Jose','Bautista'],['Josh','Donaldson'],['David','Price']] 
+urls = []
 
-def urlgenerator2(firstname,lastname):
-  a = 'http://www.baseball-reference.com/players/'
-  b = lastname[0]
-  c = lastname[0:5]
-  d = firstname[0:2]
-  e = '02'
-  f = '01'
-  g = '.shtml'
-  z = '/'
-  url = a+b+z+c+d+e+g
-  urls.append(url.lower())  
+def urlgenerator2(name):
+    for (x,y) in name:    
+        a = 'http://www.baseball-reference.com/players/'
+        b = y[0]
+        c = y[0:5]
+        d = x[0:2]
+        e = '02'
+        f = '01'
+        g = '.shtml'
+        z = '/'
+        url = a+b+z+c+d+e+g
+        urls.append(url.lower())  
+        
+def urlgenerator1(name):
+    for (x,y) in name:    
+        a = 'http://www.baseball-reference.com/players/'
+        b = y[0]
+        c = y[0:5]
+        d = x[0:2]
+        e = '02'
+        f = '01'
+        g = '.shtml'
+        z = '/'
+        url = a+b+z+c+d+f+g
+        urls.append(url.lower())  
+
+urlgenerator2(names)
+print (urls)
+
 
 //http://www.baseball-reference.com/players/b/bautijo02.shtml
 
-def urlgenerator1(firstname,lastname)
-  a = 'http://www.baseball-reference.com/players/'
-  b = lastname[0]
-  c = lastname[0:5]
-  d = firstname[0:2]
-  e = '02'
-  f = '01'
-  g = '.shtml'
-  z = '/'
-  url = a+b+z+c+d+f+g
-  print (url.lower())
 
 page = urllib.request.urlopen("http://www.baseball-reference.com/players/b/bautijo02.shtml")
 #http://www.baseball-reference.com/players/b/bautijo02.shtml
