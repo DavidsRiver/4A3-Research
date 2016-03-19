e4278aac30901d2d4499812302295dff27276e5d
DavidsRiver <riosdj@mcmaster.ca>
2016-03-19 13:59:07 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -36,17 +36,21 @@ def urlgenerator1(name):
 
 urlgenerator2(names)
 print (urls)
-#this doesnt work makes them lsits 
-soups = []
-def getsoups(x):
-    for y in x:
-        page = urllib.request.urlopen('x')
-        soups.append(page)
 
-
-WinsAbove = []
-urls= []
-def WARhitters(name):
+def urlgenerator2(name):
+    for (x,y) in name:    
+        a = 'http://www.baseball-reference.com/players/'
+        b = y[0]
+        c = y[0:5]
+        d = x[0:2]
+        e = '02'
+        f = '01'
+        g = '.shtml'
+        z = '/'
+        url = a+b+z+c+d+e+g
+        urls.append(url.lower())  
+        
+def urlgenerator1(name):
     for (x,y) in name:    
         a = 'http://www.baseball-reference.com/players/'
         b = y[0]
@@ -58,10 +62,13 @@ def WARhitters(name):
         z = '/'
         url = a+b+z+c+d+f+g
         urls.append(url.lower())  
-    for x in urls: 
-        page = urllib.request.urlopen(url)
+
+WinsAbove = []      
+urlgenerator2(names)
+def WAR(x):
+    for y in x:
+        page = urllib.request.urlopen(y)
         soup = BeautifulSoup(page.read())
-        print (soup.pretify())
         table = soup.find_all("table",{"id":"batting_value"})
         soup2 = BeautifulSoup(str(table))
         tfooter = soup2.find("tfoot")
@@ -69,19 +76,10 @@ def WARhitters(name):
         tr = soup3.find_all("td",{"align":"right"})
         lastsoup = BeautifulSoup(str(tr))
         war = lastsoup.find_all(text=True)
-        WinsAbove.append(war[23])
-    
+        print(war)
+        WinsAbove.append((war[1])[23])
 
-page = urllib.request.urlopen('http://www.baseball-reference.com/players/b/bautijo02.shtml')
-soup = BeautifulSoup(page.read())
-print (soup.prettify())
 
-page = urllib.request.urlopen('http://www.baseball-reference.com/players/d/donaljo02.shtml')
-soup = BeautifulSoup(page.read(), "lxml")
-#print (soup.prettify())
-
-WinsAbove = []
-#x is soups 
 def wargenerator(x):
     table = x.find_all("table",{"id":"batting_value"})
     soup2 = BeautifulSoup(str(table))
@@ -91,4 +89,3 @@ def wargenerator(x):
     lastsoup = BeautifulSoup(str(tr))
     war = lastsoup.find_all(text=True)
     WinsAbove.append(war[23])
-
