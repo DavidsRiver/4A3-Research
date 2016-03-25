bbf084e335784dcfd416d649f520f1b831e59912
DavidsRiver <riosdj@mcmaster.ca>
2016-03-25 19:06:53 -0400

    Update PyScrap2.py

--- PyScrap2.py
+++ PyScrap2.py
@@ -1,23 +1,27 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-page = urllib.request.urlopen('http://www.baseball-reference.com/players/g/goldspa01.shtml')
-soup = BeautifulSoup(page.read(), "lxml")
-table = soup.find("table",{"id":"batting_value"})
+#Player URL, WAR Years. 
 
-rows = table.find_all('tr')
+def playerwar(url,years):
+    page = urllib.request.urlopen(str(url))
+    soup = BeautifulSoup(page.read(), "lxml")
+    table = soup.find("table",{"id":"batting_value"})
+    rows = table.find_all('tr')
+    data = []
+    for tr in rows:
+        cols = tr.findAll('td')
+        for td in cols:
+            text = td.find(text=True) 
+            data.append(text)
+    x =(years*24)+15
+    i = 15
+    for i in range(0,x):
+        if (i - 15)%24 == 0:
+            print((data[i])+(','), end=" ") 
+            i = i + 1 
+        else:
+            i = i + 1 
+            
+playerwar('http://www.baseball-reference.com/players/b/bautijo02.shtml',5)
 
-data = []
-for tr in rows:
-    cols = tr.findAll('td')
-    for td in cols:
-        text = td.find(text=True) 
-        data.append(text)
-
-i = 15
-for i in range(0,len(data)):
-    if (i - 15)%24 == 0:
-        print(data[i], end=" ") 
-        i = i + 1 
-    else:
-        i = i + 1 
